3a11d727718c0aae57b7d7d6e16b7d5e21f0fba6
yakimchukyaana <yakimchukyaana@gmail.com>
2023-09-09 16:20:45 +0500

    add hello test

--- src/test/java/properties/SystemPropertiesTests.java
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -3,6 +3,8 @@ package properties;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.String.format;
+
 public class SystemPropertiesTests {
 
     @Test
@@ -10,5 +12,14 @@ public class SystemPropertiesTests {
     void systemPropertiesTests(){
         String browser = System.getProperty("browser", "mozilla");
         System.out.println(browser);
+    }
+
+    @Test
+    @Tag("hello")
+    void systemPropertiesTests1(){
+        String name = System.getProperty("name","default student");
+        String message = format("Hello, %s!", name);
+
+        System.out.println(message);
     }
 }
